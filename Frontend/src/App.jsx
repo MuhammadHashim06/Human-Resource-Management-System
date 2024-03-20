03236688effc89b5428fc366f3f7833c8af40908
MuhammadHashim06 <muhammad.hashimsid@gmail.com>
2024-03-20 22:11:07 +0500

    update css

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,20 @@
-import Home from "./Components/Home"
+// import Home from "./Components/Home"
 import Sidebar from "./Components/Sidebar"
-import './App.css'
+// import './App.css'
+import { useState } from "react"
 
 function App() {
+  const [Component, setComponent] = useState('Home');
+
+  function  onClick(name) {
+    console.log("Button clicked: ",name);
+    setComponent(name);
+   }
   return (
     <div className="Dasboard">
     {/* <Login/> */}
-    <Sidebar/>
-    <Home/>
+    <Sidebar prop= {onClick} />
+    <Component/>
     </div>
   )
 }
--- Frontend/src/Components/Sidebar.jsx
+++ Frontend/src/Components/Sidebar.jsx
@@ -1,25 +1,31 @@
 import './Sidebar.css'
-function Sidebar() {
-    return (
-        <div className="sidebar">
-            <div className="user">
-                <img src="../Images/Log-in_vector.png" alt="" />
-                <p>
-                    Muhammad Hashim <br />
-                    <span>Web developer</span>
-                </p>
-            </div>
-            <div className="menubar">
-                <ul>
-                    <li className="menu-link"><a href="Dashboard.html" className="menu-link">Home</a></li>
-                    <li><a href="Attendance.html" className="menu-link">Attendance</a></li>
-                    <li><a href="Assignments.html" className="menu-link">Assignments</a></li>
-                    <li><a href="Applications.html" className="menu-link">Applications</a></li>
-                    <li><a href="/index.html" className="menu-link">Log out</a></li>
-                </ul>
-            </div>
-        </div>
-    );
+
+function Sidebar(props) {
+  const handleClick = (e) => {
+    const componentName = e.target.id;
+    props.onClick(componentName);
+  };
+
+  return (
+    <div className="sidebar">
+      <div className="user">
+        <img src="../Images/Log-in_vector.png" alt="" />
+        <p>
+          Muhammad Hashim <br />
+          <span>Web developer</span>
+        </p>
+      </div>
+      <div className="menubar">
+        <ul>
+          <li id="Home" className="menu-link" onClick={handleClick}>Home</li>
+          <li id="Attendance" className="menu-link" onClick={handleClick}>Attendance</li>
+          <li id="Assignments" className="menu-link" onClick={handleClick}>Assignments</li>
+          <li id="Applications" className="menu-link" onClick={handleClick}>Applications</li>
+          <li id="Login" className="menu-link" onClick={handleClick}>Log out</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
